4d5d1ec5fa48888ef2590b62d4670728de17efb1
ambroisegithub <muhayimana21@gmail.com>
2024-06-26 08:21:18 +0200

    chore of project initialization

---
+++ app/(components)/DoctorsDashbord/DoctorsDashbord.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DoctorsDashbord = () => {
+  return (
+    <View>
+      <Text>DoctorsDashbord</Text>
+    </View>
+  )
+}
+
+export default DoctorsDashbord
